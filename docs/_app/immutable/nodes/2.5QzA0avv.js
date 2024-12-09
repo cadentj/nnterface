178f2fe7e2a320776abcedcf6a283a483f86b683
Caden <35741667+cadentj@users.noreply.github.com>
2024-12-09 01:24:11 -0500

    Test static pages

---
+++ docs/_app/immutable/nodes/2.5QzA0avv.js
@@ -0,0 +1,14 @@
+import{a as o,t as v}from"../chunks/disclose-version.Bp00QCWI.js";import"../chunks/legacy.CmmGGpQD.js";import{z as p,x as a,y as e,v as f,A as u,i as c}from"../chunks/runtime.C0v5yhm0.js";import{s as b}from"../chunks/render.BttR0iOz.js";import{e as g,i as h,s as w}from"../chunks/attributes.Snaig5n6.js";var y=v('<li class="text-left p-3 my-2 border-white border rounded-lg svelte-1xtc6m"><a class="svelte-1xtc6m"> </a></li>'),_=v(`<div class="editor-container flex flex-col items-center justify-center svelte-1xtc6m"><pre class="text-border  text-white text-2xl transform  z-10 svelte-1xtc6m">          ,--.                                
+        ,--.'|    ,---,       ,---,    ,---,. 
+    ,--,:  : |  .'  .' \`\\  ,\`--.' |  ,'  .' | 
+ ,\`--.'\`|  ' :,---.'     \\ |   :  :,---.'   | 
+ |   :  :  | ||   |  .\`\\  |:   |  '|   |   .' 
+ :   |   \\ | ::   : |  '  ||   :  |:   :  :   
+ |   : '  '; ||   ' '  ;  :'   '  ;:   |  |-, 
+ '   ' ;.    ;'   | ;  .  ||   |  ||   :  ;/| 
+ |   | | \\   ||   | :  |  ''   :  ;|   |   .' 
+ '   : |  ; .''   : | /  ; |   |  ''   :  '   
+ |   | '\`--'  |   | '\` ,/  '   :  ||   |  |   
+ '   : |      ;   :  .'    ;   |.' |   :  \\   
+ ;   |.'      |   ,.'      '---'   |   | ,'   
+ '---'        '---'                \`----'     </pre> <ul class="text-center w-[20vw] z-10 svelte-1xtc6m"></ul> <div class="gradient-overlay svelte-1xtc6m"></div> <div class="dark-overlay svelte-1xtc6m"></div></div>`);function q(m){let n=["new","lens","node-connections"];var t=_(),l=p(a(t),2);g(l,5,()=>n,h,(x,i)=>{var s=y(),r=a(s),d=a(r,!0);e(r),e(s),f(()=>{w(r,"href",`/editor/${c(i)??""}`),b(d,c(i))}),o(x,s)}),e(l),u(4),e(t),o(m,t)}export{q as component};
